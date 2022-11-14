f15099c8d3967b0e03e4173235606e656a2d724a
pavan092001 <pavant9689@gmail.com>
2022-11-14 14:37:40 +0530

    First understanding

---
+++ BASIC/src/Hokage.java
@@ -0,0 +1,11 @@
+
+public class Hokage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s ="Naruto";
+		System.out.println(s+" "+s.length());
+
+	}
+
+}
